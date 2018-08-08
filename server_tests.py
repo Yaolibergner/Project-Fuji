02a73fa4de26d0a890473243415dc310e8860194
Yao Li <melissayyl@gmail.com>
2018-08-08 23:00:02 +0000

    Completed no database related route testing.

--- server_tests.py
+++ server_tests.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 from fujiserver import app
-from model import connect_to_db, db
-# , example_data
+from model import connect_to_db, db, example_data
 from flask import session
 
 
@@ -34,116 +33,72 @@ class FlaskTestsBasic(TestCase):
                                   follow_redirects=True)
         self.assertIn(b"<h2>Yao, Welcome to Fuji Chat</h2>", result.data)
 
+        result_1 = self.client.post("/login",
+                                  data={"email": "yao@yao.com", "password": "2345"},
+                                  follow_redirects=True)
+        self.assertIn(b"Invalid password, please try again!", result_1.data)
 
-# class FlaskTestsDatabase(TestCase):
-#     """Flask tests that use the database."""
+        result_2 = self.client.post("/login",
+                                  data={"email": "yaoli@yao.com", "password": "1234"},
+                                  follow_redirects=True)
+        self.assertIn(b"<h1>Register</h1>", result_2.data)
 
-#     def setUp(self):
-#         """Stuff to do before every test."""
+    def test_register(self):
+        """Test register page.""" 
 
-#         # Get the Flask test client
-#         self.client = app.test_client()
-#         app.config['TESTING'] = True
+        result = self.client.get("/register")
+        self.assertIn(b"<h1>Register</h1>", result.data)
 
-#         # Connect to fuji database
-#         connect_to_db(app, "postgresql:///fuji")
-
-#         # Create tables and add sample data
-#         db.create_all()
-#         example_data()
-
-#     def tearDown(self):
-#         """Do at end of every test."""
-
-#         db.session.remove()
-#         db.drop_all()
-#         db.engine.dispose()
-
-#     def test_departments_list(self):
-#         """Test departments page."""
-
-#         result = self.client.get("/departments")
-#         self.assertIn(b"Legal", result.data)
-
-#     def test_departments_details(self):
-#         """Test departments page."""
-
-#         result = self.client.get("/department/fin")
-#         self.assertIn(b"Phone: 555-1000", result.data)
+    
+    def test_feedpage(self):
+        """Test feedpage form, test form not suppose to show: not logged in."""
 
 
-# class FlaskTestsLoggedIn(TestCase):
-#     """Flask tests with user logged in to session."""
+        result = self.client.get("/feedpage")
+        self.assertNotIn(b"<h1>Register</h1>", result.data)
+        
 
-#     def setUp(self):
-#         """Stuff to do before every test."""
+    def test_logout(self):
+        """Test logout route."""
 
-#         app.config['TESTING'] = True
-#         app.config['SECRET_KEY'] = 'key'
-#         self.client = app.test_client()
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user_id'] = '1'
 
-#         with self.client as c:
-#             with c.session_transaction() as sess:
-#                 sess['user_id'] = 1
+            result = self.client.get('/logout', follow_redirects=True)
 
-#     def test_important_page(self):
-#         """Test important page."""
+            self.assertNotIn(b'user_id', session)
+            self.assertIn(b'You are logged out, see you soon.', result.data)
 
-#         result = self.client.get("/important")
-#         self.assertIn(b"You are a valued user", result.data)
+class TestsDatabase(TestCase):
+    """Flask tests that use the database."""
 
+    def setUp(self):
+        """Stuff to do before every test."""
 
-# class FlaskTestsLoggedOut(TestCase):
-#     """Flask tests with user logged in to session."""
+        # Get the Flask test client
+        self.client = app.test_client()
 
-#     def setUp(self):
-#         """Stuff to do before every test."""
+        # Show Flask errors that happen during tests
+        app.config['TESTING'] = True
 
-#         app.config['TESTING'] = True
-#         self.client = app.test_client()
+        # Connect to test database
+        connect_to_db(app, "postgresql:///testdb")
 
-#     def test_important_page(self):
-#         """Test that user can't see important page when logged out."""
+        # Create tables and add sample data
+        db.create_all()
+        example_data()
 
-#         result = self.client.get("/important", follow_redirects=True)
-#         self.assertNotIn(b"You are a valued user", result.data)
-#         self.assertIn(b"You must be logged in", result.data)
+    def tearDown(self):
+        """Do at end of every test."""
 
+        db.session.close()
+        db.drop_all()
 
-# class FlaskTestsLogInLogOut(TestCase):  # Bonus example. Not in lecture.
-#     """Test log in and log out."""
+    def test_db(self):
+        """Test data added to db"""
 
-#     def setUp(self):
-#         """Before every test"""
-
-#         app.config['TESTING'] = True
-#         self.client = app.test_client()
-
-#     def test_login(self):
-#         """Test log in form.
-
-#         Unlike login test above, 'with' is necessary here in order to refer to session.
-#         """
-
-#         with self.client as c:
-#             result = c.post('/login',
-#                             data={'user_id': '42', 'password': 'abc'},
-#                             follow_redirects=True
-#                             )
-#             self.assertEqual(session['user_id'], '42')
-#             self.assertIn(b"You are a valued user", result.data)
-
-#     def test_logout(self):
-#         """Test logout route."""
-
-#         with self.client as c:
-#             with c.session_transaction() as sess:
-#                 sess['user_id'] = '42'
-
-#             result = self.client.get('/logout', follow_redirects=True)
-
-#             self.assertNotIn(b'user_id', session)
-#             self.assertIn(b'Logged Out', result.data)
+        # How to delete added registration from database???
 
 
 if __name__ == "__main__":
